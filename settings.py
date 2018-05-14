bef5bcd1d035d4f06df082381bb4777294cb9d71
gravetaxon <38481413+gravetaxon@users.noreply.github.com>
2018-05-13 20:44:26 -0700

    Corrected spelling and added cv calculations

--- settings.py
+++ settings.py
@@ -1,9 +1,11 @@
 SigmaStep = 1.5
 DecisionStep =0.51
-NumberRoutines=10
-KernelSizes=[97,47,179,17]
+NumberRoutines=5
+KernelSizes=[5,3,13,2]
 HiddenLayers=2
-FilterSize=[256,256,256,64]
+FilterSize=[8,8,8,8]
 Loss='binary_crossentropy'
-Saved=[]
+Saved=[0,1,2,3,4]
+
+
 dataShape=(1500,972,1296)
