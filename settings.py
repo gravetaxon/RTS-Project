7f6e05be25874c52343bbb48a89e24ef1a439cc7
gravetaxon <koenig2@pdx.edu>
2018-07-09 11:17:44 -0700

    Updates

--- settings.py
+++ settings.py
@@ -5,22 +5,22 @@ RTSKernelSizes=[5,3,103,2]
 RTSHiddenLayers=2
 RTSFilterSize=[8,8,8,8]
 RTSLoss='binary_crossentropy'
-RTSdataShape=(1500,972,1296)
+RTSdataShape=(1500,1944,2592)
 NRTSKernelSizes=[5,3,17,2]
 NRTSHiddenLayers=2
 NRTSFilterSize=[8,8,8,8]
 NRTSLoss='binary_crossentropy'
-NRTSdataShape=(1500,972,1296)
+NRTSdataShape=(1500,1944,2592)
 MRTSKernelSizes=[7,5,3,41,2]
 MRTSHiddenLayers=3
 MRTSFilterSize=[64,64,64,64,64]
 MRTSLoss='binary_crossentropy'
-MRTSdataShape=(1500,972,1296)
+MRTSdataShape=(1500,1944,2592)
 ERTSKernelSizes=[5,3,367,2]
 ERTSHiddenLayers=2
 ERTSFilterSize=[8,8,8,8]
 ERTSLoss='binary_crossentropy'
-ERTSdataShape=(1500,972,1296)
+ERTSdataShape=(1500,1944,2592)
 RTSHiddenLayers=2
 NRTSHiddenLayers=2
 MRTSHiddenLayers=3
@@ -43,12 +43,16 @@ MRTSHiddenLayers=3
 ERTSHiddenLayers=2
 RTSSaved=[0,1,2,3,4]
 
-MRTSSaved=[0,1,2,3,4]
+MRTSSaved=[0,2,3,4]
 
 ERTSSaved=[0,1,2,3,4]
 
 NRTSSaved=[0,1,2,3,4]
 
+RTSHiddenLayers=2
+NRTSHiddenLayers=2
+MRTSHiddenLayers=3
+ERTSHiddenLayers=2
 RTSHiddenLayers=2
 NRTSHiddenLayers=2
 MRTSHiddenLayers=3
