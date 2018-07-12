6c9369c0689fc6ea83ea493ab572fb994463b21e
gravetaxon <james@mechone.local>
2018-07-11 19:01:32 -0700

    Development of the voter module

--- voter.py
+++ voter.py
@@ -1,2 +1,55 @@
 # voter.py
-# load all of the model, have them to compare 
+# load all of the model, have them to compare
+
+
+# use openModel from makeOutput as basis to open the models
+import h5py
+from keras.models import load_model
+import pylab
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import loader
+import settings
+import sys
+import random
+
+
+def openModel(name):
+    if type(name) == str:
+        if name == 'RTS':
+            SavedModels =settings.RTSSaved
+        elif name == 'MRTS':
+            SavedModels =settings.MRTSSaved
+        elif name == 'ERTS':
+            SavedModels =settings.ERTSSaved
+        elif name == 'NRTS':
+            SavedModels =settings.NRTSSaved
+        else:
+            # default behavior
+            SavedModels = settings.Saved
+    # Saved Models has the model numbers that are to be loaded
+    NumModel = len(SavedModels)
+    DataShape = settings.dataShape
+    TestPercent = 0.01 # set this to the percentage of the data you want to use
+    loopData = (int(DataShape[0]),int(DataShape[1]*TestPercent),int(DataShape[2]*TestPercent))
+    ModelsUsed =str(name)+":"
+    for each in SavedModels:
+        ModelsUsed+=str(each)+','
+    ModelsUsed=ModelsUsed[:-1]
+    print ("DEBUG: Using the following models: {}".format(ModelsUsed))
+
+    print("DEBUG: Loading models...")
+    for each in SavedModels:
+        model_name = './PiCam/CNNlin_model{}.h5'.format(str(each))
+        model = load_model(model_name)
+        models.append(model)
+    return (model, len(model))
+
+def askVoter(name, modelIn, row, col):
+    # Ask a group of models from modelIn to judge on a pixel
+    # name    <- Name of the model/voter
+    # modelIn <- group of models to use
+    # row     <- what row to look at
+    # col     <- what col to look at
